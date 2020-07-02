9733bd86160ae32007e8fe15c3c1e56da1b8f3b7
Victor Oviedo <drvictoroviedo@gmail.com>
2020-07-02 00:04:44 -0400

    Cloud Firestore

--- app/src/main/java/com/drdextersoft/app/pruebasfirebasecloudfirestone/MainActivity.kt
+++ app/src/main/java/com/drdextersoft/app/pruebasfirebasecloudfirestone/MainActivity.kt
@@ -1,10 +1,8 @@
 package com.drdextersoft.app.pruebasfirebasecloudfirestone
 
+import android.annotation.SuppressLint
 import android.os.Bundle
-import android.util.Log
-import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
-import androidx.fragment.app.FragmentActivity
 import com.google.firebase.firestore.FirebaseFirestore
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -12,6 +10,7 @@ import kotlinx.android.synthetic.main.activity_main.*
 var db = FirebaseFirestore.getInstance()
 
 class MainActivity : AppCompatActivity() {
+    @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -28,8 +27,8 @@ class MainActivity : AppCompatActivity() {
             .get()
             .addOnSuccessListener { result ->
                 for (document in result) {
-                    texto1.text=""+texto1.text+document.data.get("texto1").toString()+"\n"
-                    texto2.text=""+texto2.text+document.data.get("texto2").toString()+"\n"
+                    texto1.text=""+texto1.text+ document.data["texto1"].toString()+"\n"
+                    texto2.text=""+texto2.text+ document.data["texto2"].toString()+"\n"
                 }
             }
     }
